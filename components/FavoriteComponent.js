ced89f2051e6ea95d2b2d2b9a82adf6546001769
Arpit <arpitmalik66@gmail.com>
2021-01-14 21:51:51 +0530

    swipeable

--- components/FavoriteComponent.js
+++ components/FavoriteComponent.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Text, FlatList } from 'react-native'
-import { ListItem, Avatar } from 'react-native-elements'
-import SwipeOut from 'react-native-swipeout'
+import { FlatList } from 'react-native'
+import CustomListItem from '../customElements/CustomListItem'
 import Loading from './LoadingComponent'
 import { deleteFavorite } from '../redux/ActionCreators'
-import { baseUrl } from '../shared/baseUrl'
 import { connect } from 'react-redux'
 
 const mapStateToProps = state => {
@@ -22,46 +20,6 @@ const mapDispatchToProps = (dispatch) => {
 
 class Favorites extends Component {
   render() {
-    const renderMenuItem = ({ item, index }) => {
-      const rightButton = [
-        {
-          text: 'Delete',
-          type: 'delete',
-          onPress: () => this.props.deleteFavorite(item.id)
-        }
-      ]
-
-      return (
-        <SwipeOut
-          autoClose={true}
-          right={rightButton}
-        >
-          <ListItem
-            key={index}
-            onPress={() => this.props.navigation.navigate( 'Dish', { dishId: item.id })}
-          >
-            <Avatar
-              rounded
-              size='large'
-              source={{ uri: baseUrl + item.image }}
-            />
-            <ListItem.Content>
-              <ListItem.Title
-                style={[styles.listItemTitle]}
-              >
-                {item.name}
-              </ListItem.Title>
-              <ListItem.Subtitle
-                style={[styles.listItemSubtitle]}
-              >
-                {item.description}
-              </ListItem.Subtitle>
-            </ListItem.Content>
-          </ListItem>
-        </SwipeOut>
-      )
-    }
-
     if(this.props.dishes.isLoading) {
       return (
         <Loading />
@@ -78,21 +36,19 @@ class Favorites extends Component {
       return (
         <FlatList
           data={this.props.dishes.dishes.filter(dish => this.props.favorites.some(el => el === dish.id))}
-          renderItem={renderMenuItem}
           keyExtractor={item => item.id.toString()}
+          renderItem={({ item, index }) => (
+            <CustomListItem
+              item={item}
+              index={index}
+              navigate={this.props.navigation.navigate}
+              rightOnPress={() => this.props.deleteFavorite(item.id)}
+            />
+          )}
         />
       )
     }
   }
 }
-
-const styles = StyleSheet.create({
-  listItemTitle: {
-    fontWeight: 'bold'
-  },
-  listItemSubtitle: {
-    color: '#000' 
-  },
-})
 
 export default connect(mapStateToProps, mapDispatchToProps)(Favorites)
---
+++ customElements/CustomListItem.js
@@ -0,0 +1,117 @@
+import React from 'react'
+import { View, StyleSheet, Animated, TouchableOpacity } from 'react-native'
+import { ListItem, Avatar } from'react-native-elements'
+import Swipeable from 'react-native-gesture-handler/Swipeable'
+import { baseUrl } from '../shared/baseUrl'
+
+const LeftActions = (progress, dragX) => {
+  const scale = dragX.interpolate({
+    inputRange: [0, 100],
+    outputRange: [0, 1],
+    extrapolate: 'clamp'
+  })
+  return (
+    <View 
+      style={styles.leftAction}
+    >
+      <Animated.Text 
+        style={[
+          styles.actionText, 
+          { 
+            transform: [{ scale }] 
+          }
+        ]}
+      >
+        Add to Cart
+      </Animated.Text>
+    </View>
+  )
+}
+
+const RightActions = ({ progress, dragX, onPress }) => {
+  const scale = dragX.interpolate({
+    inputRange: [-100, 0],
+    outputRange: [1, 0],
+    extrapolate: 'clamp'
+  })
+  return (
+    <TouchableOpacity  
+      style={[styles.rightAction]}
+      onPress={onPress}
+    >
+      <View>
+        <Animated.Text 
+          style={[
+            styles.actionText, 
+            { 
+              transform: [{ scale }] 
+            }
+          ]}
+        >
+          Delete
+        </Animated.Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+const CustomListItem = ({ item, index, navigate, rightOnPress }) => (
+  <Swipeable
+    renderRightActions={(progress, dragX) => (
+      <RightActions 
+        progress={progress} 
+        dragX={dragX} 
+        onPress={rightOnPress} 
+      />
+    )}
+  >
+    <ListItem
+      key={index}
+      onPress={() => navigate( 'Dish', { dishId: item.id })}
+    >
+      <Avatar
+        rounded
+        size='large'
+        source={{ uri: baseUrl + item.image }}
+      />
+      <ListItem.Content>
+        <ListItem.Title
+          style={[styles.listItemTitle]}
+        >
+          {item.name}
+        </ListItem.Title>
+        <ListItem.Subtitle
+          style={[styles.listItemSubtitle]}
+        >
+          {item.description}
+        </ListItem.Subtitle>
+      </ListItem.Content>
+    </ListItem>
+  </Swipeable>
+)
+
+const styles = StyleSheet.create({
+  leftAction: {
+    backgroundColor: '#388e3c',
+    justifyContent: 'center'
+  },
+  rightAction: {
+    backgroundColor: '#dd2c00',
+    justifyContent: 'center'
+  },
+  actionText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+    paddingLeft: 20,
+    paddingRight: 20
+  },
+  listItemTitle: {
+    fontWeight: 'bold'
+  },
+  listItemSubtitle: {
+    color: '#000' 
+  }
+});
+
+export default CustomListItem
